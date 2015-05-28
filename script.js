1b7d74aa976f42f0a31f61bbe7d3fa45ec9fe19a
Michael Trestman <michael.a.trestman@gmail.com>
2015-05-28 09:40:26 -0700

    separates text selection and whole text as return values of getTextInput function

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 $(function(){
   var plurals = [];
+  var activePlural = null;
 
   $('#textInput').on('keydown', function(e){
+
     // ok so this really needs to check for a combo of keypresses. we're going to do "`" as the base key, "{" and "}" as switch up and down through the variants of the current plural, "\" as save new variant, and "delete" as delete active variant;
     if (e.keyCode === 192) {
       e.preventDefault();
       var textContent = getTextInput(this);
-      renderTextOutput(this, textContent);
+      // console.log(selection);
+      renderTextOutput(this, textContent.allText);
     }
   });
 })
 
 function getTextInput(that){
- var selectionText = that.value.substr(that.selectionStart, (that.selectionEnd - that.selectionStart) );
- console.log(selectionText);
- var textBeforeSelection = that.value.substr(0, that.selectionStart);
- var textAfterSelection = that.value.substr(that.selectionEnd, that.value.length);
- that.value = textBeforeSelection + "~{" + selectionText + "}~" + textAfterSelection;
- return that.value;
+  var selectionText = that.value.substr(that.selectionStart, (that.selectionEnd - that.selectionStart) );
+  console.log(selectionText);
+  var textBeforeSelection = that.value.substr(0, that.selectionStart);
+  var textAfterSelection = that.value.substr(that.selectionEnd, that.value.length);
+  that.value = textBeforeSelection + "~{" + selectionText + "}~" + textAfterSelection;
+  return {
+    allText: that.value,
+    selection: selectionText
+  };
 }
 
 function renderTextOutput(that, textContent){
- textContent = textContent.replace(/~\{/g, '<span class="plural">');
- textContent = textContent.replace(/\}~/g, '</span>');
- console.log(textContent);
- $('#textOutput').html(textContent);
+  textContent = textContent.replace(/~\{/g, '<span class="plural">');
+  textContent = textContent.replace(/\}~/g, '</span>');
+  console.log(textContent);
+  $('#textOutput').html(textContent);
+
 }
